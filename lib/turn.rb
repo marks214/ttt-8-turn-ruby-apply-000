9a9ee552431b3e7d7492449aa58cbf953f721f5d
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-28 00:18:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,12 @@ end
 def input_to_index(input)
   input = input.to_i - 1
 end
+
+def valid_move(board, index)
+  if board[index] == "" || board[index] == " "
+    true
+  elsif index < 0 || index > 8 || board[index] == "X" || board[index] == "O"
+    false
+  end
+    
+end
